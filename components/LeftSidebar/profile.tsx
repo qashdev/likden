dc57619e5842fc6f5e421517d2a1a16f0a0ec6bd
Qasir Khan <qasirkhan9852@gmail.com>
2026-04-09 17:27:09 +0530

    profile

--- components/LeftSidebar/profile.tsx
+++ components/LeftSidebar/profile.tsx
@@ -2,16 +2,16 @@ function Profile() {
   return (
 
     
-    <div className="bg-white relative rounded-xl shadow-sm text-black text-xs ">
-      <div className="relative">
-      <img src="/icons/003.jpg" alt="profile" className="flex-fit full h-20 object-cover rounded-xl" />
+    <div className="bg-white relative shadow-sm text-black text-xs w-[100%] ">
+      <div className="relative Flex items-center justify-center ">
+      <img src="/icons/003.jpg" alt="background" className="flex items-center justify-center w-full h-16 object-cover rounded-t-xl Flex items-center justify-center " />
       <img src="/icons/002.jpg" alt="profile" className="w-20 h-20 rounded-full absolute top-10 left-5.5 " />
       </div>
-      <div className="mt-10 px-3 " >
-      <h1 className="font-semibold mb-2 text-lg">QASIR KHAN </h1>
-      <p className="text-sm">Full Stack Developer | Building Fast, Scalable, User-Focused Web Products | From Idea to Deployment</p>
-      <p className="text-sm text-gray-600">Delhi, India</p>
-      <div className="flex items-center gap-2">
+      <div className="mt-10  pl-4 py-4 " >
+      <h1 className="font-semibold  text-lg">QASIR KHAN </h1>
+      <p className="text-sm-sm">Full Stack Developer | Building Fast, Scalable, User-Focused Web ...</p>
+      <p className="text-sm-sm-sm text-gray-600 pt-1">Delhi, India</p>
+      <div className="flex items-center gap-1 pt-2 ">
       <img src="/icons/004.jpg" alt="profile" className="w-5 h-5 rounded-full" />
       <p className="text font-semibold ">Himotech Global PVT. LTD </p>
       </div>
